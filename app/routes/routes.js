e286b76416d275590ffc5a042074f356fc9a19b4
saroj kumar swain <sarojkumar@applaunch.io>
2020-08-23 23:15:20 +0530

    Controller added

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,10 +4,4 @@ module.exports = (app) => {
     app.post('/product/create', product.create);
 
     app.get('/product/all', product.findAll);
-
-    app.get('/product/:productId', product.findOne);
-
-    app.put('/product/:productId', product.update);
-
-    app.delete('/product/:productId', product.delete);
 }
